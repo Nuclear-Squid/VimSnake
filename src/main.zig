2a25400dadb686e67436f8ffaa8b7a3dd88f7cb3
Léo Cazenave <leo@cazenave.cc>
2023-09-11 14:55:03 +0200

    Cleaner code structure with window as a standalone struct.

--- src/main.zig
+++ src/main.zig
@@ -1,79 +1,105 @@
 const std = @import("std");
 const os  = std.os;
 
+const Window = struct {
+    tty              : std.fs.File,
+    width            : u16,
+    height           : u16,
+    original_termios : os.system.termios,
+    termios          : os.system.termios,
+
+    fn init() !@This() {
+        // Get acces to the terminal window
+        var tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .read_write });
+
+        // Get terminal size, manually handle exit status because the api is weird
+        var size = std.mem.zeroes(os.system.winsize);
+        const status = os.system.ioctl(tty.handle, os.system.T.IOCGWINSZ, @intFromPtr(&size));
+        if (os.errno(status) != .SUCCESS) {
+            return os.unexpectedErrno(@enumFromInt(status));
+        }
+
+        // Get termios to "uncook" tty. Keep original to set tty to its original state later
+        const original_termios = try os.tcgetattr(tty.handle);
+        var termios = original_termios;
+
+        // Uncook termios
+        termios.lflag &= ~@as(os.system.tcflag_t,
+            os.system.ECHO   |  // Stop the terminal from displaying pressed keys.
+            os.system.ICANON |  // Allows reading inputs byte-wise instead of line-wise.
+            os.system.ISIG   |  // Disable signals for Ctrl-C and Ctrl-Z.
+            os.system.IEXTEN    // Disable input preprocessing to handle Ctrl-V.
+        );
+
+        termios.iflag &= ~@as(os.system.tcflag_t,
+            os.system.IXON   |  // Disable software control flow for Ctrl-S and Ctrl-Q.
+            os.system.ICRNL  |  // Disable carriage returns to handle Ctrl-J and Ctrl-M.
+            // The following flags are likely to have no effect on any modern terminal
+            os.system.BRKINT |  // Disable converting sending SIGINT on break conditions.
+            os.system.INPCK  |  // Disable parity checking.
+            os.system.ISTRIP    // Disable stripping the 8th bit of characters.
+        );
+
+        // Disable output processing.
+        termios.oflag &= ~@as(os.system.tcflag_t, os.system.OPOST);
+
+        // Set the character size to 8 bits per byte. Likely has no efffect on
+        // anything remotely modern.
+        termios.cflag |= os.system.CS8;
+
+        // Syscall stuff, don’t really get it.
+        termios.cc[os.system.V.TIME] = 0;
+        termios.cc[os.system.V.MIN]  = 1;
+
+        // Apply changes
+        try os.tcsetattr(tty.handle, .FLUSH, termios);
+
+        // Create Tui window
+        try tty.writer().writeAll(
+            "\x1B[?25l"   ++ // Hide the cursor.
+            "\x1B[s"      ++ // Save cursor position.
+            "\x1B[?47h"   ++ // Save screen.
+            "\x1B[?1049h"    // Enable alternative buffer.
+        );
+
+        return @This() {
+            .tty     = tty,
+            .height  = size.ws_row,
+            .width   = size.ws_col,
+            .original_termios = original_termios,
+            .termios = termios
+        };
+    }
+
+    fn deinit(self: *@This()) !void {
+        // reset original termios
+        try os.tcsetattr(self.*.tty.handle, .FLUSH, self.*.original_termios);
+        // return to previous terminal view
+        try self.*.tty.writer().writeAll(
+            "\x1B[?25h"   ++ // Show the cursor.
+            "\x1B[?1049l" ++ // Disable alternative buffer.
+            "\x1B[?47l"   ++ // Restore screen.
+            "\x1B[u"         // Restore cursor position.
+        );
+    }
+};
+
 pub fn main() !void {
-    var tty = try std.fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
-    defer tty.close();
+    var window = try Window.init();
+    defer window.deinit() catch {};
 
-    const original_termios = try os.tcgetattr(tty.handle);
-    try uncookTermios(&tty, original_termios);
-    defer recookTermios(&tty, original_termios) catch {};
-
-    try tty.writer().writeAll("Hello World!");
-
+    const writer = window.tty.writer();
+    for ("Hello World!") |char| {
+        std.time.sleep(100_000_000);  // Sleep takes nanoseconds
+        try writer.writeByte(char);
+    }
+    
     while (true) {
         var buffer: [1]u8 = undefined;
-        _ = try tty.read(&buffer);
+        _ = try window.tty.read(&buffer);
         if (buffer[0] == 'q') {
             return;
         }
+        try writer.writeAll(&buffer);
     }
-}
-
-fn recookTermios(tty: *std.fs.File, original_termios: os.termios) !void {
-    try os.tcsetattr(tty.*.handle, .FLUSH, original_termios);
-    var writer = tty.writer();
-    try writer.writeAll("\x1B[?25h");   // Show the cursor.
-    try writer.writeAll("\x1B[?1049l"); // Disable alternative buffer.
-    try writer.writeAll("\x1B[?47l");   // Restore screen.
-    try writer.writeAll("\x1B[u");      // Restore cursor position.
-}
-
-fn uncookTermios(tty: *std.fs.File, base_termios: os.termios) !void {
-    var termios = base_termios;
-    //   ECHO: Stop the terminal from displaying pressed keys.
-    // ICANON: Disable canonical ("cooked") input mode. Allows us to read inputs
-    //         byte-wise instead of line-wise.
-    //   ISIG: Disable signals for Ctrl-C (SIGINT) and Ctrl-Z (SIGTSTP), so we
-    //         can handle them as "normal" escape sequences.
-    // IEXTEN: Disable input preprocessing. This allows us to handle Ctrl-V,
-    //         which would otherwise be intercepted by some terminals.
-    termios.lflag &= ~@as(
-        os.system.tcflag_t,
-        os.system.ECHO   | os.system.ICANON | os.system.ISIG   | os.system.IEXTEN
-    );
-
-    //   IXON: Disable software control flow. This allows us to handle Ctrl-S
-    //         and Ctrl-Q.
-    //  ICRNL: Disable converting carriage returns to newlines. Allows us to
-    //         handle Ctrl-J and Ctrl-M.
-    // BRKINT: Disable converting sending SIGINT on break conditions. Likely has
-    //         no effect on anything remotely modern.
-    //  INPCK: Disable parity checking. Likely has no effect on anything
-    //         remotely modern.
-    // ISTRIP: Disable stripping the 8th bit of characters. Likely has no effect
-    //         on anything remotely modern.
-    termios.iflag &= ~@as(
-        os.system.tcflag_t,
-        os.system.IXON   | os.system.ICRNL  | os.system.BRKINT | os.system.INPCK  | os.system.ISTRIP
-    );
-
-    // Disable output processing. Common output processing includes prefixing
-    // newline with a carriage return.
-    termios.oflag &= ~@as(os.system.tcflag_t, os.system.OPOST);
-
-    // Set the character size to 8 bits per byte. Likely has no efffect on
-    // anything remotely modern.
-    termios.cflag |= os.system.CS8;
-
-    termios.cc[os.system.V.TIME] = 0;
-    termios.cc[os.system.V.MIN]  = 0;
-
-    try os.tcsetattr(tty.*.handle, .FLUSH, termios);
-
-    var writer = tty.writer();
-    try writer.writeAll("\x1B[?25l");   // Hide the cursor.
-    try writer.writeAll("\x1B[s");      // Save cursor position.
-    try writer.writeAll("\x1B[?47h");   // Save screen.
-    try writer.writeAll("\x1B[?1049h"); // Enable alternative buffer.
 }
